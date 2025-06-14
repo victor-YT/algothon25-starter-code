22bc3e8fb15543237d3993940d563ee8cfe8559c
UNSW-FinTech-Society-IT <it.unswfintechsoc@gmail.com>
2025-06-14 21:49:49 +1000

    Update eval.py

--- eval.py
+++ eval.py
@@ -2,27 +2,24 @@
 
 import numpy as np
 import pandas as pd
-from main import getMyPosition as getPosition
+from teamName import getMyPosition as getPosition
 
 nInst = 0
 nt = 0
-commRate = 0.0010
+commRate = 0.0005
 dlrPosLimit = 10000
-
 
 def loadPrices(fn):
     global nt, nInst
-    df = pd.read_csv(fn, sep='\s+', header=None, index_col=None)
-    (nt, nInst) = df.shape
+    df=pd.read_csv(fn, sep='\s+', header=None, index_col=None)
+    (nt,nInst) = df.shape
     return (df.values).T
 
-
-pricesFile = "./prices.txt"
+pricesFile="./priceSlice_test.txt"
 prcAll = loadPrices(pricesFile)
-print("Loaded %d instruments for %d days" % (nInst, nt))
+print ("Loaded %d instruments for %d days" % (nInst, nt))
 
-
-def calcPL(prcHist):
+def calcPL(prcHist, numTestDays):
     cash = 0
     curPos = np.zeros(nInst)
     totDVolume = 0
@@ -30,43 +27,49 @@ def calcPL(prcHist):
     totDVolumeRandom = 0
     value = 0
     todayPLL = []
-    (_, nt) = prcHist.shape
-    for t in range(751, 1001):
-        prcHistSoFar = prcHist[:, :t]
-        newPosOrig = getPosition(prcHistSoFar)
-        curPrices = prcHistSoFar[:, -1]
-        posLimits = np.array([int(x) for x in dlrPosLimit / curPrices])
-        newPos = np.clip(newPosOrig, -posLimits, posLimits)
-        deltaPos = newPos - curPos
-        dvolumes = curPrices * np.abs(deltaPos)
-        dvolume = np.sum(dvolumes)
-        totDVolume += dvolume
-        comm = dvolume * commRate
-        cash -= curPrices.dot(deltaPos) + comm
+    (_,nt) = prcHist.shape
+    startDay = nt + 1 - numTestDays
+    for t in range(startDay, nt+1):
+        prcHistSoFar = prcHist[:,:t]
+        curPrices = prcHistSoFar[:,-1]
+        if (t < nt):
+            # Trading, do not do it on the very last day of the test
+            newPosOrig = getPosition(prcHistSoFar)
+            posLimits = np.array([int(x) for x in dlrPosLimit / curPrices])
+            newPos = np.clip(newPosOrig, -posLimits, posLimits)
+            deltaPos = newPos - curPos
+            dvolumes = curPrices * np.abs(deltaPos)
+            dvolume = np.sum(dvolumes)
+            totDVolume += dvolume
+            comm = dvolume * commRate
+            cash -= curPrices.dot(deltaPos) + comm
+        else:
+            newPos = np.array(curPos)
         curPos = np.array(newPos)
         posValue = curPos.dot(curPrices)
         todayPL = cash + posValue - value
-        todayPLL.append(todayPL)
         value = cash + posValue
         ret = 0.0
         if (totDVolume > 0):
             ret = value / totDVolume
-        print("Day %d value: %.2lf todayPL: $%.2lf $-traded: %.0lf return: %.5lf" %
-              (t, value, todayPL, totDVolume, ret))
+        if (t > startDay):
+            print ("Day %d value: %.2lf todayPL: $%.2lf $-traded: %.0lf return: %.5lf" % (t,value, todayPL, totDVolume, ret))
+            todayPLL.append(todayPL)
     pll = np.array(todayPLL)
-    (plmu, plstd) = (np.mean(pll), np.std(pll))
+    (plmu,plstd) = (np.mean(pll), np.std(pll))
     annSharpe = 0.0
     if (plstd > 0):
-        annSharpe = np.sqrt(250) * plmu / plstd
+        annSharpe = np.sqrt(249) * plmu / plstd
     return (plmu, ret, plstd, annSharpe, totDVolume)
 
 
-(meanpl, ret, plstd, sharpe, dvol) = calcPL(prcAll)
+
+(meanpl, ret, plstd, sharpe, dvol) = calcPL(prcAll,200)
 score = meanpl - 0.1*plstd
-print("=====")
-print("mean(PL): %.1lf" % meanpl)
-print("return: %.5lf" % ret)
-print("StdDev(PL): %.2lf" % plstd)
-print("annSharpe(PL): %.2lf " % sharpe)
-print("totDvolume: %.0lf " % dvol)
-print("Score: %.2lf" % score)
+print ("=====")
+print ("mean(PL): %.1lf" % meanpl)
+print ("return: %.5lf" % ret)
+print ("StdDev(PL): %.2lf" % plstd)
+print ("annSharpe(PL): %.2lf " % sharpe)
+print ("totDvolume: %.0lf " % dvol)
+print ("Score: %.2lf" % score)
