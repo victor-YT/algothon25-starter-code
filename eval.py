c03b65b12b48a00a5fac6ff85865ddd6be94ea76
Victor_YT <liyutong0918@outlook.com>
2025-06-28 17:05:22 +1000

    1

--- eval.py
+++ eval.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-from teamName import getMyPosition as getPosition
+from main import getMyPosition as getPosition
 
 nInst = 0
 nt = 0
@@ -15,7 +15,7 @@ def loadPrices(fn):
     (nt,nInst) = df.shape
     return (df.values).T
 
-pricesFile="./priceSlice_test.txt"
+pricesFile="./prices.txt"
 prcAll = loadPrices(pricesFile)
 print ("Loaded %d instruments for %d days" % (nInst, nt))
 
